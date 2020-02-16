ea088cf2ef9c5fa6677dd84686f9ab1abdaae062
Andrew J Casarsa <acasarsa@gmail.com>
2020-02-16 22:44:51 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,82 @@
+require "pry"
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+    puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
-end
-
-def hit?
-  # code hit? here
+  card_total = 0
+  2.times do
+    card_total += deal_card #1 random (1..11)
+  end
+  display_card_total(card_total)
+  card_total
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
+
+def hit?(card_total)
+  prompt_user
+  input = get_user_input
+
+  # until input == "h" || input == "s"
+  #   invalid_command
+  #   prompt_user
+  #   input = get_user_input
+  # end
+
+  valid_inputs = ["h", "s"]
+  until valid_inputs.include?(input)
+    invalid_command
+    prompt_user
+    input = get_user_input
+  end
+
+  if input == "h" # == is the comparison
+    card_total += deal_card
+     # should this be deal_card? like how am i getting this new random card_value if this was a real program and not a test
+  elsif input == "s"
+    card_total
+  end
+end
+
+
 
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  card_total = initial_round #so only using hit inside this method so therefore hit does have acces to card_total via initial round
+
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+
+  if card_total > 21
+    end_game(card_total)
+  end
 end
-    
